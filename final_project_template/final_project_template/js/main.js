fec582c31d6ebc482eaef4b980019fe8734c5225
ravinderdeepgill <ravinderdeep.gill@gmail.com>
2017-08-13 23:22:02 -0700

    Final Project (Without JS Coded)

---
+++ final_project_template/final_project_template/js/main.js
@@ -0,0 +1,18 @@
+// Make it rain!
+console.log('Hello from JS!');
+
+
+//When the user clicks the hambuger icon with a class of .hamburger
+  //display Home, About, Projects, and Contects as a drop down
+
+
+//When the user clicks on the angle down button/link with a class of .fa-angle-down
+  //Create  a jumplink so it takes the user to the About Me Section with some type of transition
+
+
+//When the user hovers/clicks over a particular project.
+  //zoom in on that image
+  //display bullet points on what I did  and hide any other projects that exist
+
+//As the user scrolls down to a particular section
+  //Highlight & Underline that particular section in the nav bar
